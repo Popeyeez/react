14953871df1f0c1c95fac162bbf626945a309cd3
Popeyeez <lozo022011@gmail.com>
2025-08-18 12:17:01 +0800

    end

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,62 @@
-import Image from "next/image";
-
-export default function Home() {
+const Home = () => {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="flex">
+      <div className="pt-10">
+        <div class="w-120 h-125 rounded-md border-black">
+          <p class="text-indigo-700 pt-10 pl-10 text-2xl ">Hobby</p>
+          <div className="flex gap-[15px] pl-10 pt-3">
+            <div className="flex gap-3">
+              <p className="text-5xl text-black font-bold"> 29$ </p>
+              <p className="text-black pt-5"> /month</p>
+            </div>
+          </div>
+          <div className="pl-10 pt-5">
+            <p className="text-black">
+              The perfect plan if you're just getting started with our product.
+            </p>
+          </div>
+          <div className="text-black pt-12 pl-10 flex-col flex gap-4">
+            <p>25 products</p>
+            <p>Up to 10,000 subscribers</p>
+            <p>Advanced analytics</p>
+            <p>24-hour support response time</p>
+          </div>
+          <div className="flex justify-center align-center pt-13 content-center">
+            <div className="w-100 bg-white-400 h-10 flex justify-center rounded-xl align-center text-indigo-400 items-center ">
+              Get started today
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div class="w-120 h-145 bg-gray-900 rounded-2xl">
+        <p class="text-indigo-400 pt-10 pl-10 text-xl">Enterprise</p>
+        <div className="flex gap-[15px] pl-10 pt-3">
+          <div className="flex gap-3">
+            <p className="text-5xl text-white font-bold"> 99$ </p>
+            <p className="text-white pt-5"> /month</p>
+          </div>
+        </div>
+        <div className="pl-10 pt-5">
+          <p className="text-white">
+            Dedicated support and infrastructure for your company.
+          </p>
+        </div>
+        <div className="text-white pt-12 pl-10 flex-col flex gap-4">
+          <p>Unlimited products</p>
+          <p>Unlimited subscribers</p>
+          <p>Advanced analytics</p>
+          <p>Dedicated support representative</p>
+          <p>Marketing automations</p>
+          <p>Custom integrations</p>
+        </div>
+        <div className="flex justify-center align-center pt-13 content-center">
+          <div className="w-100 bg-indigo-400 h-10 flex justify-center rounded-xl align-center text-white items-center">
+            Get started today
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default Home;
